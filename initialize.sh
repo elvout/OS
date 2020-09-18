138615cbd49c569e8c0f32120984e183fcfd4813
elvout <elvin.yang9@gmail.com>
2020-09-18 12:20:01 -0500

    Fix and clean up initialize.sh
    - More informative status messages
    - Fix Makefile variable expansion

--- initialize.sh
+++ initialize.sh
@@ -6,7 +6,6 @@ wd="$1"
 srcdir="$( dirname "${BASH_SOURCE[0]}" )"
 
 if [[ "$wd" == "" ]]; then
-    # wd="$(pwd)"
     wd="."
 fi
 
@@ -31,11 +30,13 @@ echo "Initializing..."
 
 
 #~~~~~~~~
-echo "Adding spamon"
+echo -n "Adding spamon ... "
 touch "${wd}/spamon"
+echo "done"
+
 
 #~~~~~~~~
-echo "Writing exclude files to .vscode/settings.json"
+echo -n "Writing exclude files to .vscode/settings.json ... "
 VSCSET="${wd}/.vscode/settings.json"
 
 [[ -d "${wd}/.vscode" ]] || mkdir "${wd}/.vscode"
@@ -46,36 +47,34 @@ else
     mv -f "$VSCSET.tmp" "$VSCSET"
     rm -f "$VSCSET.tmp"
 fi
+echo "done"
 
 
 #~~~~~~~~
-echo "Applying clang-format to .h .cc"
+echo -n "Applying clang-format to *.h *.cc ... "
 # syntax will not work on macos
 find "${wd}" -type f -regextype posix-extended -regex ".+\.(h|cc)" -exec \
     clang-format -i {} \;
-
+echo "done"
 
 
 #~~~~~~~~
-echo "Adding color messages to Makefile"
+echo -n "Adding color messages to Makefile ... "
 MKFILE="${wd}/Makefile"
-grep "PASS =" MKFILE > /dev/null
+grep "PASS :=" "$MKFILE" > /dev/null
 if [[ $? -ne 0 ]]; then
-    sed -i '1s/^/FAIL := $(shell echo "\\e[0;31mfail\\e[0m")\n/' MKFILE
-    sed -i '1s/^/PASS := $(shell echo "\\e[0;32mpass\\e[0m")\n/' MKFILE
-    sed -i 's/"pass"/"$(PASS)"/' MKFILE
-    sed -i 's/"fail"/"$(FAIL)"/' MKFILE
+    sed -i '1s/^/FAIL := $(shell echo "\\e[0;31mfail\\e[0m")\n/' "$MKFILE"
+    sed -i '1s/^/PASS := $(shell echo "\\e[0;32mpass\\e[0m")\n/' "$MKFILE"
+    sed -i 's/"pass"/"$(PASS)"/' "$MKFILE"
+    sed -i 's/"fail"/"$(FAIL)"/' "$MKFILE"
 fi
+echo "done"
+
 
 #~~~~~~~~
 echo ""
 echo "Review changes before committing."
 
-# set up mirror remote one github
-#   git remote add --mirror=push github <github url>
 
-#   probably better to push to mirror when updating lab machine
-#   so that a test commit doesn't have to run on gheith's server
-
-
-#  remember to not push branches to gheith's server
+# set up mirror on github
+#   git remote add --mirror=push github <remote url>
